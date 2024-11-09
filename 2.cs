9ab0dd128c36740f559c12073e115ed94cd62b48
GokuBlack-n1 <maksimstrutinskij875@gmail.com>
2024-11-09 17:52:27 +0200

    test2
    2

---
+++ 2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+     public Transform transformCube;
+     public Transform transformSphere;
+    public float x;
+    public float y;
+    public int speed;
+     private void Start()
+    {
+        x += 1 * Time.deltaTime;
+        y += 1 * Time.deltaTime;
+        transformCube.position = new Vector3(x, y, 25f);
+    }
+    
+    
+        
+ }
+
