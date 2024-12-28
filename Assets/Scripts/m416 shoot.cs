12262b42eb533fe0a25cf0d566013fe79eb82170
STARTIT-47\Student <academy@startit.lviv.ua>
2024-12-28 16:09:56 +0200

    ozaza

--- Assets/Scripts/m416 shoot.cs
+++ Assets/Scripts/m416 shoot.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class m416shoot : MonoBehaviour
+public class m416SHOOT : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+    
 
-    // Update is called once per frame
+public class RifleReload : MonoBehaviour
+{
+    public int currentAmmo = 0;        // Поточна кількість патронів
+    public int maxAmmo = 30;           // Максимальна кількість патронів в магазині
+    public bool isReloading = false;   // Чи виконується перезарядка
+    public float reloadTime = 2f;      // Час для перезарядки
+    public Animator animator;          // Аніматор для гвинтівки
+
     void Update()
     {
-        
+        // Перевірка, чи патрони закінчились і натискання клавіші "R"
+        if (currentAmmo == 0 && !isReloading && Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    // Корутин для перезарядки
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        // Запускаємо анімацію перезарядки
+        animator.SetTrigger("Reload");
+
+        // Чекаємо, поки анімація перезарядки завершиться
+        yield return new WaitForSeconds(reloadTime);
+
+        // Після завершення перезарядки, встановлюємо максимальну кількість патронів
+        currentAmmo = maxAmmo;
+
+        isReloading = false;
+    }
+
+    // Функція для стрільби (не обов'язкова, але для тесту)
+    public void Shoot()
+    {
+        if (currentAmmo > 0)
+        {
+            currentAmmo--;
+            // Тут додайте код для стрільби
+        }
+        else
+        {
+            Debug.Log("Патрони закінчились!");
+        }
     }
 }
+
+}
+
