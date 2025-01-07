2e471ada2e0b677aae08b54ba2be68d6e2c07b3b
STARTIT-22\Student <academy@startit.lviv.ua>
2025-01-07 14:46:06 +0200

    M4a4version 2.0
    M4a4version 2.0

--- Assets/Scripts/AK47Shooting.cs
+++ Assets/Scripts/AK47Shooting.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class AK47BalleticShooting : MonoBehaviour
 {
+    
+    public AudioSource reloadGunSound;
     public Transform bulletSpawnPoint; // Місце, де з'являються кулі (ствол)
     public GameObject bulletPrefab; // Префаб кулі
     public float bulletSpeed = 40f; // Початкова швидкість кулі
@@ -60,8 +62,7 @@ public class AK47BalleticShooting : MonoBehaviour
         Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         nextFireTime = Time.time + fireRate;
 
- 
-
+       
         // Відтворення звуку пострілу
         if (gunSound != null)
         {
